1672545477752e3edb6c1f8426b452471b11a6fe
Ali Ghazal <aligazal.wd93@gmail.com>
2026-07-27 19:36:34 +0300

    Home Deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+const baseURL = process.env.NEXT_PUBLIC_API_URL?.trim();
+
+if (!baseURL) {
+    throw new Error("NEXT_PUBLIC_API_URL is not defined");
+}
+
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    Accept: "application/json",
-  },
+    baseURL,
+    headers: {
+        Accept: "application/json",
+    },
 });
 
 export default api;
